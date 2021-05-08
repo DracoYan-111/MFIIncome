8e3637ac3185252f7d866be1af2001e9df1fbf41
29449 <2944972991@qq.com>
2021-05-08 11:37:32 +0800

    增加注释

--- contracts/MFIIncome.sol
+++ contracts/MFIIncome.sol
@@ -4,56 +4,111 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract mfiincone is Ownable {
+    //--------------------------- EVENT --------------------------
+    /*
+    MFI存款事件
+    传入 用户地址,存款数量,区块跨度
+    */
     event MFIDeposit(address userAddr, uint256 count, uint256 time);
+
+    /*
+    MFI取款事件
+    传入 用户地址,取款数量,当前区块号
+    */
     event MFIWithdrawal(address userAddr, uint256 count, uint256 time);
+
+    /*
+    MFI退款事件
+    传入 用户地址,退款数量,当前区块号
+    */
     event MFIDropOut(address userAddr, uint256 count, uint256 time);
 
     using SafeERC20 for ERC20;
     using SafeMath for uint256;
 
+    //MFI地址
     ERC20 public MfiAddress;
+    //Mfi总数
     uint256 public MFICount;
+    //存款数量(默认500个)
     uint256 public DepositCount = 500e18;
 
+    //--------------------------- STRUCT --------------------------
     struct userCount {
         uint256 userDepositCount;
         uint256 userWithdrawalCount;
         uint256 time;
     }
 
+    //--------------------------- MAPPING --------------------------
     mapping(address => userCount) userData;
 
+
+    //---------------------------ADMINISTRATOR FUNCTION --------------------------
+    /*
+    设置MFI地址
+    传入 mfi地址
+    */
     function SetMfiAddress(ERC20 _mfiAddress) external onlyOwner {
         MfiAddress = _mfiAddress;
     }
 
-    function SetUserCount(address[] memory _userAddres, uint256[] memory _userCounts) external onlyOwner {
+    /*
+    设置用户奖励总数
+    传入 用户数组,奖励数组
+    */
+    function SetUserRewardCount(address[] memory _userAddres, uint256[] memory _userCounts) external onlyOwner {
         for (uint256 i = 0; i < _userAddres.length; i++) {
             userData[_userAddres[i]].userWithdrawalCount = _userCounts[i];
         }
     }
 
+    /*
+    设置存款数量
+    传入 存款数量
+    */
     function SetDepositCount(uint256 _depositCount) external onlyOwner {
         DepositCount = _depositCount;
     }
 
+    /*
+    借用token
+    传入 用户地址,数量
+    */
     function borrow(address _userAddr, uint256 _count) external onlyOwner {
         MfiAddress.safeTransfer(_userAddr, _count);
     }
 
+    //---------------------------INQUIRE FUNCTION --------------------------
+    /*
+    查看MFI用户余额
+    返回 mfi数量
+    */
     function GetUserBalance() public view returns (uint256){
         return MfiAddress.balanceOf(msg.sender);
-
     }
 
+    /*
+    查看用户质押总数
+    返回 用户质押mfi总数
+    */
     function GetUserWithdrawalCount() public view returns (uint256){
         return userData[msg.sender].userWithdrawalCount;
     }
 
+    /*
+    查看用户奖励总数
+    返回 用户奖励总数
+    */
     function GetUserDepositCount() public view returns (uint256){
         return userData[msg.sender].userDepositCount;
     }
 
+    //--------------------------- USER FUNCTION --------------------------
+    /*
+    存款
+    传入 存款数量
+    */
     function deposit(uint256 _time) external {
         require(GetUserBalance() >= DepositCount, "Insufficient balance:(");
         MfiAddress.safeIncreaseAllowance(address(this), DepositCount);
@@ -68,6 +123,9 @@ contract mfiincone is Ownable {
         emit MFIDeposit(msg.sender, userData[msg.sender].userDepositCount, userData[msg.sender].time);
     }
 
+    /*
+    退款
+    */
     function dropOut() external {
         require(userData[msg.sender].time > block.number, "Time is not up...");
         require(userData[msg.sender].userDepositCount >= DepositCount, "Insufficient count:(");
@@ -81,6 +139,10 @@ contract mfiincone is Ownable {
         userData[msg.sender].userDepositCount = 0;
     }
 
+    /*
+    取款
+    传入 取款数量
+    */
     function withdrawal(uint256 _count) external {
         require(GetUserWithdrawalCount() >= _count, "Insufficient amount of withdrawals:(");
         MfiAddress.safeTransfer(msg.sender, _count);
